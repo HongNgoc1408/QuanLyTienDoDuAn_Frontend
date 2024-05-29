4f8c97a6b11e096b8e55c910fa9651dd8b465dce
LeThiHongNgoc <ngocb2005766@student.ctu.edu.vn>
2024-05-29 14:20:45 +0700

    commit 1

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,17 +1,7 @@
-import { Layout } from "antd";
 import React from "react";
-import FooterComponent from "../../components/FooterComponent/FooterComponent";
-import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
 
 const ProfilePage = () => {
-  return (
-    <div>
-      <Layout>
-        <HeaderComponent />
-        <FooterComponent />
-      </Layout>
-    </div>
-  );
+  return <div>ProfilePage</div>;
 };
 
 export default ProfilePage;
