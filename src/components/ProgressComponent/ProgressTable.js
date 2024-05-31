e2d3af8ef307e56e58ecaac59ade914113c56482
LeThiHongNgoc <ngocb2005766@student.ctu.edu.vn>
2024-05-31 14:43:34 +0700

    hien thi va xoa tien do

--- src/components/ProgressComponent/ProgressTable.js
+++ src/components/ProgressComponent/ProgressTable.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Button, Popconfirm, Spin, Table, message } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Input, Popconfirm, Space, Spin, Table, message } from "antd";
 import { deleteProgress, getProgresses } from "../../services/ProgressService";
 import { Link } from "react-router-dom";
+import Highlighter from "react-highlight-words";
+import { SearchOutlined } from "@ant-design/icons";
 
 const ProgressTable = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-
+  const onChange = (pagination, filters, sorter, extra) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
   const onSelectChange = (newSelectedRowKeys) => {
     console.log("selectedRowKeys changed: ", newSelectedRowKeys);
     setSelectedRowKeys(newSelectedRowKeys);
@@ -21,53 +25,208 @@ const ProgressTable = () => {
       Table.SELECTION_NONE,
     ],
   };
-  const columns = [
-    {
-      title: "STT",
-      dataIndex: "index",
-      key: "1",
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            Close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1677ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
     },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: "#ffc069",
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns = [
+    { key: "1", title: "STT", dataIndex: "index" },
     {
+      key: "2",
       title: "Tên dự án",
       dataIndex: "title",
-      key: "2",
+
+      ...getColumnSearchProps("title"),
     },
     {
+      key: "3",
       title: "Mô tả dự án",
       dataIndex: "description",
-      key: "3",
+
+      ...getColumnSearchProps("description"),
     },
     {
+      key: "4",
       title: "Người được giao",
       dataIndex: "assigned_to",
-      key: "4",
+
+      ...getColumnSearchProps("assigned_to"),
     },
     {
+      key: "5",
       title: "Trạng thái",
       dataIndex: "status",
-      key: "5",
+      filters: [
+        {
+          text: "Chưa bắt đầu",
+          value: "not_started",
+        },
+        {
+          text: "Đang tiến hành",
+          value: "in_progress",
+        },
+        {
+          text: "Hoàn thành",
+          value: "completed",
+        },
+        {
+          text: "Bị hủy",
+          value: "cancel",
+        },
+      ],
+      onFilter: (value, record) => record.status.startsWith(value),
+      filterSearch: true,
     },
     {
+      key: "6",
       title: "Độ ưu tiên",
       dataIndex: "priority",
-      key: "6",
+      filters: [
+        {
+          text: "Thấp",
+          value: "low",
+        },
+        {
+          text: "Trung bình",
+          value: "medium",
+        },
+        {
+          text: "Cao",
+          value: "high",
+        },
+      ],
+      onFilter: (value, record) => record.priority.startsWith(value),
+      filterSearch: true,
     },
     {
+      key: "7",
       title: "Ngày bắt đầu",
       dataIndex: "start_date",
-      key: "7",
+      ...getColumnSearchProps("start_date"),
     },
     {
+      key: "8",
       title: "Ngày kết thúc",
       dataIndex: "end_date",
-      key: "8",
+      ...getColumnSearchProps("end_date"),
     },
     {
+      key: "9",
       title: "Hoạt động",
       dataIndex: "actions",
-      key: "9",
       fixed: "right",
-
       render: (_, record) => (
         <span>
           <Link to={`edit/${record.key}`}>
@@ -135,6 +294,7 @@ const ProgressTable = () => {
   return (
     <>
       <Table
+        onChange={onChange}
         rowSelection={rowSelection}
         columns={columns}
         dataSource={data}
